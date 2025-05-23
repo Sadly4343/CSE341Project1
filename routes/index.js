97967c7d2023a7e8315e1408b134f081af73d1d5
Sadly4343 <loganhartshorn@gmail.com>
2025-05-22 21:00:17 -0600

    updates

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 
-router.use('/', require('./swagger'));
+router.use('/', require('./swagger')); // This is now safe
 
 router.get('/', (req, res) => {
-    res.send('hello world')
-})
+    res.send('hello world');
+});
 
 router.use('/cars', require('./cars'));
-
 router.use('/clients', require('./clients'));
 
 module.exports = router;
